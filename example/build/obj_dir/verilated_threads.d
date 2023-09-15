b976f668127a652514eb352995f8b6a2f8a9a55a
LiuZhetan <liuzhetande@foxmail.com>
2023-09-15 23:30:36 +0800

    add .gitignore

--- example/build/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated_threads.cpp \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated_threads.h \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated.h \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/ubuntu_wsl/oss-cad-suite/share/verilator/include/verilated_funcs.h
